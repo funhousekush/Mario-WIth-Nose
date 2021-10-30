079575fb6693d04c2f0281099623e089e81dc6f6
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-10-30 18:48:41 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -52,9 +52,11 @@ var gameConfig={
 /*====================================
 =            Game Status             =
 ====================================*/
-
+NoseX = ""
+NoseY = ""
+GameStatus = ""
 function game(){
-
+  console.log("Nose's X Position = " + NoseX + " ; " + "Nose's Y Position = " + NoseY + " ;");
   instializeInDraw();
   moveEnvironment(mario);
   drawSprites();
@@ -67,7 +69,7 @@ function game(){
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press Any Arrow Keys to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text("Press Start button to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -76,6 +78,7 @@ function game(){
 
     changeGameStatud();
   }
+  
   
   if(gameConfig.status==='play'){
     positionOfCharacter(mario);
@@ -88,6 +91,7 @@ function game(){
     // autoControl(mario);
   
   }
+
 
     // if game is over 
   if(gameConfig.status==='gameover'){
@@ -113,9 +117,14 @@ function game(){
 }  
 
 
+function startGAME()
+{
+  GameStatus = "start";
+  document.getElementById("status").innerHTML = "Game has loaded";
+}
 // change game status if any key is pressed
 function changeGameStatud(character){
-  if((keyDown(control.up) ||keyDown(control.left)||keyDown(control.right) )&& gameConfig.status==="start") {
+  if(GameStatus == "start" && NoseX != "" && NoseY != "" && gameConfig.status==="start") {
     world_start.play();
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
@@ -213,6 +222,7 @@ function getCoins(coin,character){
   if( character.overlap(coin) && character.live && coin.get==false){
     character.coins+=1;
     coin.get=true;
+    Mario_Coin.play();
   };
 }
     
@@ -279,13 +289,13 @@ function autoControl(character){
 function manualControl(character){
   
   if(character.live){
-    if(keyDown(control.left)){
+    if(NoseX <= 300){
       character.velocity.x-=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(-1);
     }
 
-    if(keyDown(control.right)){
+    if(NoseX > 300){
       character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
@@ -300,8 +310,9 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if( (keyWentDown(control.up)&&character.live) || (touchIsDown&&character.live) ){
+	if( (NoseY <= 180 && character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
+    Mario_jump.play();
 	}
 }
 
@@ -356,6 +367,7 @@ function StepOnEnemy(obj1,obj2){
     }else{
       obj1.velocity.y+=gameConfig.jump*0.8;
     }
+    Mario_kill.play();
 	}
 }
 
@@ -368,6 +380,10 @@ function die(character){
     character.status="dead";
     character.changeAnimation('dead');
     character.velocity.y-=2;
+    if(character.liveNumber > 0)
+    {
+      Mario_die.play();
+    }
 }
 
 // check character status and response to sprite and game status
@@ -378,9 +394,9 @@ function checkStatus(character){
     reviveAfterMusic(character);
   }
   if(character.live==false && character.liveNumber==0){
-    gameConfig.status="gameover"
+    gameConfig.status="gameover";
+    Gameover.play();
   }
-
 }
 
 // revive after dying music finished
--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+NoseX = "";
+NoseY = "";
 function preload() {
 	world_start = loadSound("world_start.wav");
+	Mario_die = loadSound("mariodie.wav");
+	Gameover = loadSound("gameover.wav");
+	Mario_jump = loadSound("jump.wav");
+	Mario_Coin = loadSound("coin.wav");
+	Mario_kill = loadSound("kick.wav");
 	setSprites();
 	MarioAnimation();
 }
@@ -7,11 +14,37 @@ function preload() {
 function setup() {
 	canvas = createCanvas(1240,336);
 	instializeInSetup(mario);
-	canvas.parent("Gamer_div")
+	canvas.parent("Gamer_div");
+
+	video = createCapture(VIDEO);
+	video.size(600, 300);
+	video.parent("Console_div");
+
+	poseNet = ml5.poseNet(video , modelLoaded);
+	poseNet.on('pose' , gotResults);
+}
+function modelLoaded()
+{
+	console.log("Model has loaded :)");
+}
+
+function gotResults(results, error)
+{
+	if(results<= 0)
+	{
+		console.error(error);
+	}
+	else
+	{
+		console.log(results);
+        NoseX = results[0].pose.nose.x;
+		NoseY = results[0].pose.nose.y;
+		console.log("Nose's X Position = " + NoseX + " ; " + "Nose's Y Position = " + NoseY + " ;");
+	}
 }
 
 function draw() {
-	game()
+	game();
 }
 
 
